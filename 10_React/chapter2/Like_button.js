b89b8ebfabefc02975a1fcc060cb42c56aacac7d
yunjeong0204 <dua0204@naver.com>
2023-05-23 17:40:00 +0900

    리액트

--- 10_React/chapter2/Like_button.js
+++ 10_React/chapter2/Like_button.js
@@ -1,8 +1,14 @@
 // 리액트 함수 컴포넌트
 // only JS로만 짠 코드(JSX 사용 안 함)
 function LikeButton() {
-  const [isClicked, setIsClicked] =React.useState(false);
-  return React.createElement{
-    'button'
-  }
-}
+  const [isClicked, setIsClicked] = React.useState(false);
+  return React.createElement( 
+    'button',
+    {onClick: () => setIsClicked(true)},
+    isClicked ? 'you liked this.' : 'Like'
+  );
+};
+
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement(LikeButton));
