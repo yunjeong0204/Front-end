33d7a51c73ba1cac773920f0abb51787aaf10075
yunjeong0204 <dua0204@naver.com>
2023-05-23 17:31:03 +0900

    리액트

--- 10_React/chapter2/Like_button.js
+++ 10_React/chapter2/Like_button.js
@@ -1,2 +1,8 @@
 // 리액트 함수 컴포넌트
 // only JS로만 짠 코드(JSX 사용 안 함)
+function LikeButton() {
+  const [isClicked, setIsClicked] =React.useState(false);
+  return React.createElement{
+    'button'
+  }
+}
