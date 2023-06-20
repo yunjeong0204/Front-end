f67df40de0a33f1cf5166c274bbfe06aa0d89585
yunjeong0204 <dua0204@naver.com>
2023-06-20 18:55:01 +0900

    복습

--- 10_React/my-news/src/components/Jsonplaceholder.jsx
+++ 10_React/my-news/src/components/Jsonplaceholder.jsx
@@ -2,48 +2,30 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-// 복습 jsx
+const MovieItem = styled.div`
+  display: flex;
+`;
+
+// 영화API
 function Jsonplaceholder(props) {
-  const ListBlock = styled.div`
-    width: 768px;
-    margin: 0 auto;
-  `;
+  const {movieCd, movieNm, openDt} = article;
   const [data, setData] = useState();
 
   const handleREquest =  async () => {
     try {
-      const response = await axios.get(`https://www.kobis.or.kr/kobisopenapi/homepg/main/main.do`);
+      const response = await axios.get(` http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml`);
       setData(response.moveList.moveNm);
     } catch (error) {
       console.error();
     }
   }
   return (
-    <ListBlock>
-      <button type='button' onClick={() => {handleREquest}}></button>
-    </ListBlock>
+    <MovieItem>
+      <p>{movieNm}</p>
+    </MovieItem>
   );
 }
 
 // http://json.parser.online.fr/
 
 export default Jsonplaceholder;
-// </header>
-// <body>
-//     <dataType>XML</dataType>
-//     <items>
-//         <item>
-//             <icaoCode>RKJB</icaoCode>
-//             <airportName>무안공항</airportName>
-//             <tmFc>202109120000</tmFc>
-//             <wd>30</wd>
-//             <ws>6</ws>
-//             <ta>24</ta>
-//             <qnh>2988</qnh>
-//         </item>
-//     </items>
-//     <numOfRows>10</numOfRows>
-//     <pageNo>1</pageNo>
-//     <totalCount>4</totalCount>
-// </body>
-// </response>
